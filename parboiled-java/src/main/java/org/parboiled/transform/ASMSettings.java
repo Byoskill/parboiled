407f73e270dedacd7f3cd3c15f1848770595779e
Sylvain Leroy <sleroy0@gmail.com>
2021-02-02 20:18:55 +0100

    min version Java 1.8 and ASM 8

--- parboiled-java/src/main/java/org/parboiled/transform/ASMSettings.java
+++ parboiled-java/src/main/java/org/parboiled/transform/ASMSettings.java
@@ -6,9 +6,8 @@ import org.objectweb.asm.Opcodes;
 /**
  *
  */
-public class ASMSettings
-  {
-  public static final int ASM_API = Opcodes.ASM7;
-  public static final int JDK_VERSION = Opcodes.V1_7;
-  public static final int FRAMES = ClassWriter.COMPUTE_FRAMES;
-  }
+public class ASMSettings {
+    public static final int ASM_API     = Opcodes.ASM8;
+    public static final int JDK_VERSION = Opcodes.V1_8;
+    public static final int FRAMES      = ClassWriter.COMPUTE_FRAMES;
+}
